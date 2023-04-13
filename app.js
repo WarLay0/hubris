3ebfb0d893cefd9ededffad35a65af714420c1fa
axel <axelbreheret@gmail.com>
2023-04-13 16:59:15 +0200

    test febrile

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-$(document).ready(function(){
+ $(document).ready(function(){
 
     // Disparition des images au survol des liens
-    $('#icare').hover(function(){
-        //animation dans le css
-        setTimeout(()=>{
-            $('#bgIcare').css('display', 'none');
-        }, 000)
+ //   $('#icare').hover(function(){
+ //       //animation dans le css
+ //       setTimeout(()=>{
+ //           $('#bgIcare').css('display', 'none');
+   //     }, 000)
 
-    }, function(){
-        $('#bgIcare').css('display', 'block');
-    });
+  //  }, function(){
+   //     $('#bgIcare').css('display', 'block');
+   // });
 
-    $('#narcisse').hover(function(){
-        $('.narcisseName').css('left', '53vw');
-        $('.narcisseName').css('opacity', '0');
+//     $('#narcisse').hover(function(){
+//         $('.narcisseName').css('left', '53vw');
+//         $('.narcisseName').css('opacity', '0');
 
-        $('.narcisseImage').css('left', '100vw');
-        $('.narcisseImage').css('opacity', '0');
+//         $('.narcisseImage').css('left', '100vw');
+//         $('.narcisseImage').css('opacity', '0');
 
-        $('#bgNarcisse').css('opacity', '0');
+//         $('#bgNarcisse').css('opacity', '0');
 
-        setTimeout(()=>{
-            $('#bgNarcisse').css('display', 'none');
-            $('.content').css('opacity', '1');
-        }, 1000)
-    }, function(){
+//         setTimeout(()=>{
+//             $('#bgNarcisse').css('display', 'none');
+//             $('.content').css('opacity', '1');
+//         }, 1000)
+//     }, function(){
 
-        $('#bgNarcisse').css('display', 'block');
-        $('#bgNarcisse').css('opacity', '1');
-        $('#bgNarcisse').css('background-color', 'transparent');
+//         $('#bgNarcisse').css('display', 'block');
+//         $('#bgNarcisse').css('opacity', '1');
+//         $('#bgNarcisse').css('background-color', 'transparent');
 
-        $('.narcisseImage').css('left', '100vw');
+//         $('.narcisseImage').css('left', '100vw');
 
-        setTimeout(()=>{
-            $('.narcisseImage').css('left', '75vw');
-            $('.narcisseImage').css('opacity', '1');
+//         setTimeout(()=>{
+//             $('.narcisseImage').css('left', '75vw');
+//             $('.narcisseImage').css('opacity', '1');
 
-            $('.narcisseName').css('left', '58vw');
-            $('.narcisseName').css('opacity', '1');
-        }, 200)
-        setTimeout(()=>{
-            $('#bgNarcisse').css('background-color', 'black');
-        }, 500)
+//             $('.narcisseName').css('left', '58vw');
+//             $('.narcisseName').css('opacity', '1');
+//         }, 200)
+//         setTimeout(()=>{
+//             $('#bgNarcisse').css('background-color', 'black');
+//         }, 500)
 
-        if($('#bgNarcisse').css('display', 'none')){
-            $('#bgNarcisse').css('display', 'block');
-            setTimeout(()=>{
-                $('#bgNarcisse').css('display', 'block');
-            }, 1000)
-        }
-    });
+//         if($('#bgNarcisse').css('display', 'none')){
+//             $('#bgNarcisse').css('display', 'block');
+//             setTimeout(()=>{
+//                 $('#bgNarcisse').css('display', 'block');
+//             }, 1000)
+//         }
+//     });
 
     
 
-    $('#midas').hover(function(){
-        $('#bgMidas').css('display', 'none');
-    }, function(){
-        $('#bgMidas').css('display', 'block');
-    });
+//     $('#midas').hover(function(){
+//         $('#bgMidas').css('display', 'none');
+//     }, function(){
+//         $('#bgMidas').css('display', 'block');
+//     });
 
-    // Scoller à un endroit précis dans la page, 100vh
-    $('#headerToMain').click(function(){
+//     // Scoller à un endroit précis dans la page, 100vh
+     $('#headerToMain').click(function(){
         $('html, body').animate({
-            scrollTop: $('#main').offset().top
-        }, 500 );        
-    })
-})
+             scrollTop: $('#main').offset().top
+         }, 500 );        
+     })
+ })
